12e20b1446ec47996db1dd1e3f4fcc38ff355121
MarcusMellorMIM <marcus@mellor.com>
2019-05-13 10:53:17 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Marcus"
+  end
+
+  get '/hometown' do
+    "My hometown is Orpington"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Charlotte by the Mac Lads"
+  end
 end
